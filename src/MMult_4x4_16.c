322866695ecdf057e8a395757d56af2200e978eb
Jed Brown <jed@jedbrown.org>
2017-01-23 14:28:25 -0700

    Add AVX2/FMA and OpenMP implementations

---
+++ src/MMult_4x4_16.c
@@ -0,0 +1,130 @@
+/* Create macros so that the matrices are stored in column-major order */
+
+#define A(i,j) a[ (j)*lda + (i) ]
+#define B(i,j) b[ (j)*ldb + (i) ]
+#define C(i,j) c[ (j)*ldc + (i) ]
+
+/* Block sizes */
+#define mc 256
+#define kc 128
+#define nb 1000
+
+#define min( i, j ) ( (i)<(j) ? (i): (j) )
+
+/* Routine for computing C = A * B + C */
+
+void AddDot4x4( int, double *, int, double *, int, double *, int );
+void PackMatrixA( int, double *, int, double * );
+void PackMatrixB( int, double *, int, double * );
+void InnerKernel( int, int, int, double *, int, double *, int, double *, int, int );
+
+void MY_MMult( int m, int n, int k, double *a, int lda, 
+                                    double *b, int ldb,
+                                    double *c, int ldc )
+{
+  int i, p, pb, ib;
+
+  /* This time, we compute a mc x n block of C by a call to the InnerKernel */
+
+  for ( p=0; p<k; p+=kc ){
+    pb = min( k-p, kc );
+    for ( i=0; i<m; i+=mc ){
+      ib = min( m-i, mc );
+      InnerKernel( ib, n, pb, &A( i,p ), lda, &B(p, 0 ), ldb, &C( i,0 ), ldc, i==0 );
+    }
+  }
+}
+
+void InnerKernel( int m, int n, int k, double *a, int lda, 
+                                       double *b, int ldb,
+                                       double *c, int ldc, int first_time )
+{
+  int i, j;
+  double 
+    packedA[ m * k ] __attribute__((aligned(64)));
+  static double 
+    packedB[ kc*nb ] __attribute__((aligned(64)));    /* Note: using a static buffer is not thread safe... */
+
+  for ( j=0; j<n; j+=4 ){        /* Loop over the columns of C, unrolled by 4 */
+    if ( first_time )
+      PackMatrixB( k, &B( 0, j ), ldb, &packedB[ j*k ] );
+    for ( i=0; i<m; i+=4 ){        /* Loop over the rows of C */
+      /* Update C( i,j ), C( i,j+1 ), C( i,j+2 ), and C( i,j+3 ) in
+	 one routine (four inner products) */
+      if ( j == 0 ) 
+	PackMatrixA( k, &A( i, 0 ), lda, &packedA[ i*k ] );
+      AddDot4x4( k, &packedA[ i*k ], 4, &packedB[ j*k ], k, &C( i,j ), ldc );
+    }
+  }
+}
+
+void PackMatrixA( int k, double *a, int lda, double *a_to )
+{
+  int j;
+
+  for( j=0; j<k; j++){  /* loop over columns of A */
+    double 
+      *a_ij_pntr = &A( 0, j );
+
+    *a_to     = *a_ij_pntr;
+    *(a_to+1) = *(a_ij_pntr+1);
+    *(a_to+2) = *(a_ij_pntr+2);
+    *(a_to+3) = *(a_ij_pntr+3);
+
+    a_to += 4;
+  }
+}
+
+void PackMatrixB( int k, double *b, int ldb, double *b_to )
+{
+  int i;
+  double 
+    *b_i0_pntr = &B( 0, 0 ), *b_i1_pntr = &B( 0, 1 ),
+    *b_i2_pntr = &B( 0, 2 ), *b_i3_pntr = &B( 0, 3 );
+
+  for( i=0; i<k; i++){  /* loop over rows of B */
+    *b_to++ = *b_i0_pntr++;
+    *b_to++ = *b_i1_pntr++;
+    *b_to++ = *b_i2_pntr++;
+    *b_to++ = *b_i3_pntr++;
+  }
+}
+
+#include <immintrin.h>
+
+void AddDot4x4( int k, double *a, int lda,  double *b, int ldb, double *c, int ldc )
+{
+  /* So, this routine computes a 4x4 block of matrix A
+
+           C( 0, 0 ), C( 0, 1 ), C( 0, 2 ), C( 0, 3 ).  
+           C( 1, 0 ), C( 1, 1 ), C( 1, 2 ), C( 1, 3 ).  
+           C( 2, 0 ), C( 2, 1 ), C( 2, 2 ), C( 2, 3 ).  
+           C( 3, 0 ), C( 3, 1 ), C( 3, 2 ), C( 3, 3 ).  
+
+     Notice that this routine is called with c = C( i, j ) in the
+     previous routine, so these are actually the elements 
+
+           C( i  , j ), C( i  , j+1 ), C( i  , j+2 ), C( i  , j+3 ) 
+           C( i+1, j ), C( i+1, j+1 ), C( i+1, j+2 ), C( i+1, j+3 ) 
+           C( i+2, j ), C( i+2, j+1 ), C( i+2, j+2 ), C( i+2, j+3 ) 
+           C( i+3, j ), C( i+3, j+1 ), C( i+3, j+2 ), C( i+3, j+3 ) 
+	  
+     in the original matrix C 
+
+     And now we use vector registers and instructions */
+
+  int i,p;
+  __m256d vc[4];
+  for (i=0; i<4; i++) {
+    vc[i] = _mm256_loadu_pd(&c[i*ldc]);
+  }
+  for (p=0; p<k; p++) {
+    __m256d va = _mm256_load_pd(&a[p*lda]);
+    for (i=0; i<4; i++) {
+      vc[i] += va * b[p*4+i];
+    }
+  }
+  for (i=0; i<4; i++) {
+    _mm256_storeu_pd(&c[i*ldc],vc[i]);
+  }
+}
